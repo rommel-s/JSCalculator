c8f890620f7d9f3033db0aaf95078a0d8ef8f617
Rommel Santos <rommelrenne@gmail.com>
2020-04-19 18:14:18 -0300

    Added main JavaScript file

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function calcHistory() {
+  return document.getElementById('history-value').innerText
+}
+
+function showHistory(nbr) {
+  document.getElementById('history-value').innerText = nbr
+}
+
+function getOutput() {
+  return document.getElementById('output-value').innerText
+}
+
+function showOutput(nbr) {
+  document.getElementById('output-value').innerText = nbr
+}
+
