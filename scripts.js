bf0339c2ff286c9583b29a839f6829bf35fd4c1a
Rommel Santos <rommelrenne@gmail.com>
2020-04-23 19:05:14 -0300

    Added some ideas for the logic of calculator

--- scripts.js
+++ scripts.js
@@ -15,22 +15,72 @@ function showOutput(nbr) {
     document.getElementById('output-value').innerText = nbr
 
   } else {
-    document.getElementById('output-value').innerText = formatNumber(nbr)
+    document.getElementById('output-value').innerText = nbr
 
   }
 
 }
 
-function formatNumber(nbr) {
-  var number = Number(nbr)
-  var format = number.toLocaleString("br")
-  return format
-}
+// function formatNumber(nbr) {
+//   var number = Number(nbr)
+//   var format = number.toLocaleString("br")
+//   return format
+// }
 
-showOutput('18111111')
 // function getDefaultInput(nbr) {
 //   return Number(nbr.replace(/./g,''))
 // }
 
 // console.log(getDefaultInput(getOutput()))
 
+var operator = document.getElementsByClassName("operator")
+var number = document.getElementsByClassName("number")
+var action = document.getElementsByClassName("act-butn")
+
+//numbers output
+for (let init=0; init<number.length; init++) {
+  number[init].addEventListener('click', function() {
+    console.log("clicou em "+this.value)
+    var output = getOutput()
+    if (output != NaN) {
+      output = output + this.value
+      showOutput(output)
+    }
+  })
+}
+
+// operations output
+for (let init=0; init<operator.length; init++ ) {
+  operator[init].addEventListener('click', function() {
+    console.log("clicou em "+this.value)
+    var output = getOutput()
+    var history = calcHistory()
+    if (output != "") {
+
+      history = history + output
+      if (this.id == '=') {
+        let result = eval(output)
+        showOutput(result)
+        showHistory('')
+    } 
+    }
+  })
+}
+
+//actions
+for (let init=0; init<action.length; init++) {
+  action[init].addEventListener('click', function() {
+    console.log("clicou em "+this.value)
+    if (this.id == 'clear') {
+      showHistory('')
+      showOutput('')
+    } else if (this.id == 'backspace') {
+      let output = getOutput().toString()
+      if (output) {
+        output = output.substr(0, output.length - 1)
+        showOutput(output)
+      }
+    }
+    
+  })
+}
